6f61e510d3ef6d03de1628436bc1c0eeaa51055b
Mark Cohen <mark.simon.cohen@gmail.com>
2023-05-05 18:57:27 +0800

    first step for processing at tx

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 from urllib import request
 from zipfile import ZipFile
-from typing import Tuple
+from typing import Tuple, Set
 
 # file locations for general use
 data_dir = "../data/raw"
@@ -19,7 +19,7 @@ data_files = {
 
 def prepend_dir(filename):
     """
-    Prepnds `../data/process/` to the filename
+    Prepends `../data/processed/` to the filename
     """
     return '../data/processed/' + filename
 
@@ -94,20 +94,73 @@ def make_txdata_reader(**kwargs):
     Returns an iterator to read chunks of the main transaction data file. The default chunk size is 10,000, but alternative values for `chunksize` or for any other parameter of `pd.read_csv` can be pased as named arguments.
     """
     params = {
-        "chunksize": 100000
+        "chunksize": 100_000
     }
     params.update(kwargs)
     return pd.read_csv(data_dir+'/'+data_files["full_tx"], **params)
 
 
-def save_data(dfdict):
+def save_data(dfdict, **kwargs):
     """
     Saves data to `../data/processed`. The keys of the `datafiles` dict are taken to be the filenames, and the values are assumed to be data frames
     """
     dirname = "../data/processed"
     confirm_dirs(dirname)
     for filename, df in dfdict.items():
-        df.to_csv(dirname+'/'+filename)
+        df.to_csv(dirname+'/'+filename, **kwargs)
+
+
+def make_training_sets(subset_ids, pos_filename, neg_filename, rate):
+    """
+    Constructs and saves two training data sets from the full training data recorded in `pos_filename` and `neg_filename`:
+    - An unbalanced set containing users in `subset_ids`
+    - A balanced set combining all positive cases and negative cases selected at `rate`
+    """
+    subset_name = prepend_dir('tx_train_set.csv')
+    balanced_name = prepend_dir('tx_train_balanced.csv')
+    balanced_df = pd.read_csv(pos_filename, index_col=0)
+    subset_df = balanced_df[balanced_df.user.isn(subset_ids)].copy()
+    ...
+    # TODO
+  
+
+def clean_split_tx(users_cards_df: pd.DataFrame, test_ids):
+    """
+    Processes raw transaction data and splits into test set (comprised of users in `test_ids`) and full training set (the rest).
+    """
+    raw_data_on_disk()
+    save_file_pos = prepend_dir('tx_train_pos.csv')
+    save_file_neg = prepend_dir('tx_train_neg.csv')
+    test_file = prepend_dir('tx_test.csv')
+    pos_count = 0
+    n = 0
+    reader = make_txdata_reader()
+    # in first iteration, overwrite file and write header
+    writing_args = {'mode':'w', 'header':True}
+    for df in reader:
+        df = next(reader)
+        df = clean_tx_df(df)
+        df = df.merge(users_cards_df, how='left', on=['user', 'card'])
+        df = user_features(df)
+
+        test_df = df[df.user.isin(test_ids)]
+        test_df.to_csv(test_file, **writing_args)
+
+        train_df = df[~df.user.isin(test_ids)]
+        pos_count += train_df.is_fraud.sum()
+        n += train_df.is_fraud.count()
+        train_df[train_df.is_fraud].to_csv(save_file_pos, **writing_args)
+        train_df[~train_df.is_fraud].to_csv(save_file_neg, **writing_args)
+        
+        # after first iteration mode is append and header should not be written
+        writing_args = {'mode': 'a', 'header':False}
+    metadata = {
+        "pos_filename": save_file_pos,
+        "neg_filename": save_file_neg,
+        "rate": pos_count / n
+    }
+    return metadata
+
 
 #################
 # DATA CLEANING #
@@ -184,6 +237,29 @@ def clean_tx_df(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
+def user_features(df: pd.DataFrame) -> pd.DataFrame:
+    #location-based features
+    merged_df = df.copy()
+    merged_df['home_city'] = (merged_df.city == merged_df.merchant_city) & (merged_df.state == merged_df.merchant_state)
+    merged_df['home_state'] = merged_df.state == merged_df.merchant_state
+    merged_df['home_zip'] = merged_df.zip == merged_df.zipcode
+    merged_df['overseas'] = (merged_df.tx_type != 'online') & merged_df.zip.isna()
+    merged_df = merged_df[~(merged_df.merchant_state.isna() & (merged_df.tx_type != 'online'))]
+
+    # time-based features
+    merged_df['user_age'] = (merged_df.timestamp - merged_df.birthdate).dt.days/365
+    merged_df['retired'] = merged_df.user_age >= merged_df.retirement_age
+    merged_df['until_expired'] = (merged_df.expires - merged_df.timestamp).dt.days
+    merged_df['since_opened'] = (merged_df.timestamp - merged_df.acct_open_date).dt.days
+
+    # dropping unneeded columns
+    redundant_cols = ['merchant_city', 'merchant_state', 'zip', 'city', 'state', 'zipcode', 'birthdate', 'retirement_age', 'expires', 'acct_open_date', 'timestamp']
+    merged_df.drop(columns=redundant_cols, inplace=True)
+
+    return merged_df
+
+
+#TODO make into a scikitlearn transformer class
 def convert_multicat(df: pd.DataFrame, colname: str) -> Tuple[pd.DataFrame, list]:
     '''Copies `df`  and converts the categorical column `colname` has been converted into dummies. Allows for membership in multiple categories separated by a single comma, e.g. entry "a,b" will be converted into `True` for columns `a` and `b`'''
     dummy_df = df.copy()
